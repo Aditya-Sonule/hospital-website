ac1c8401a5bcce87664450e83dde32d9a1aa564a
AdityaSonule <aditya.sonule07@gmail.com>
2026-07-05 22:47:25 +0530

    fix render requirements and python version

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -38,12 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+
     "rest_framework",
     "corsheaders",
 
     "api",
-
 ]
 
 
